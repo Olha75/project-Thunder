0d1e1997a341b18aa8f88abcc8eaa2835cbd4d93
brsvvv <brsvpwnz@gmail.com>
2023-08-29 00:42:51 +0200

    29.08. 00:27
    Зроблена анімація.Виправлені попередні помилки. МАЄ БУТИ ВСЕ ІДЕАЛЬНО!

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-hidden');
+    mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
@@ -17,12 +17,4 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
 })();
