74b9c5379c3a8288622f1244a7cc4e4a0cdddf33
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-08-29 10:14:18 +0300

    main-js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobMenuLinks = document.querySelectorAll('.mob-menu-link'); // Доданий рядок
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,4 +18,21 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Доданий обробник для посилань
+  mobMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  });
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
